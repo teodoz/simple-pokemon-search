e8c7801e0b32edf6045dda6014a78cb9c89686e1
Saulo Teodoz <teodoz@live.com>
2026-03-20 00:22:24 -0300

    feat(api): adicionar autocomplete e remover busca direta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   pokeApi: 'https://pokeapi.co/api/v2/pokemon/',
-  pokeDex: {
-    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/',
+  pokeApiImg: {
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/',
   },
 };
